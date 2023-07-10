d437ce13aba2424707093982d0f08b65bd73a07c
Preksha Agrawal <81644899+preksha002@users.noreply.github.com>
2023-07-10 19:27:43 +0530

    Day6

---
+++ Day6/DetectCycle-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head || !head -> next)
+            return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(slow && fast  && fast -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next ;
+            if(slow == fast)
+                break;
+        }
+        if(slow != fast)
+            return NULL;
+        slow = head;
+        while(true && slow != fast){
+            slow = slow -> next ;
+            fast = fast -> next ;
+            if(slow == fast)
+                break;
+        }
+        return slow;
+    }
+};
